791aa028fe35c4b0fff6258d7d5a44d33052d169
MaitySoumitra <soumitra.ahaansoftware@gmail.com>
2026-05-21 20:03:05 +0530

    run successfully

--- developer-group/src/App.tsx
+++ developer-group/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [employee, setEmployee] = useState([])
 
   useEffect(()=>{
-    fetch('http://localhost:4000')
+    fetch('http://localhost:4000/')
     .then(res=>res.json())
     .then(data=>setEmployee(data))
   }, [])
